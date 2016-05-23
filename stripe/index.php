10a691c1eb4bdbb52559e3b9d381af282e0f42b1
Sean Rock <sean@sparxitsolutions.com>
2016-05-23 18:43:56 +0530

    stripe

---
+++ stripe/index.php
@@ -0,0 +1,13 @@
+<form action="" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_wt1RDNQLFXiSgt3uSuGuYzsp"
+    data-amount="999"
+    data-name="Demo Site"
+    data-description="Widget"
+    data-image="/img/documentation/checkout/marketplace.png"
+    data-locale="auto">
+  </script>
+</form>
+
+<a href="custom_form.php"> Payment using custom form</a>
